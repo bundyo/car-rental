5cfa3ffa6c96784cfad642d70056e0b9de7e3311
Bundyo <bundyo@gmail.com>
2018-06-08 17:20:59 +0300

    Fix firebase crash. Simplify the car service. Load the first screen with the list view.

--- app/app.js
+++ app/app.js
@@ -2,17 +2,51 @@
 // Can be moved to webpack config once upgraded to 4.x
 import "./vendor";
 
-import Vue from 'nativescript-vue';
+import Vue from "nativescript-vue";
+import firebase from "nativescript-plugin-firebase";
+import config from "./shared/firebase-config";
 
-import HelloWorld from './components/HelloWorld';
+import cars from "./shared/cars/car-service";
 
-import './styles.scss';
+import CarList from "./components/CarList";
+
+import "./styles.scss";
 
 // Uncommment the following to see NativeScript-Vue output logs
 Vue.config.silent = false;
 
 new Vue({
 
-    render: h => h(HelloWorld)
+    render: h => h(CarList),
+
+    data: {
+        cars: null
+    },
+
+    provide() {
+        const store = {};
+
+        Object.defineProperty(store, "cars", {
+           enumerable: true,
+           get: () => this.cars,
+        });
+
+        return { store };
+    },
+
+    created() {
+        firebase.init(config).then(
+            instance => {
+                console.log("firebase.init done");
+
+                cars.load().then((data) => {
+                    this.cars = Object.values(data);
+                })
+            },
+            error => {
+                console.log(`firebase.init error: ${error}`);
+            }
+        );
+    }
 
 }).$start();
