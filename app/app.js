433f7ca65e93fccca65e936bb6b2d4b4e2696e7a
Bundyo <bundyo@gmail.com>
2018-06-08 10:41:18 +0300

    Add a comment.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
+// Currently required for the app to work.
+// Can be moved to webpack config once upgraded to 4.x
 import "./vendor";
 
 import Vue from 'nativescript-vue';
 
 import HelloWorld from './components/HelloWorld';
-
 
 import './styles.scss';
 
@@ -13,6 +14,5 @@ Vue.config.silent = false;
 new Vue({
 
     render: h => h(HelloWorld)
-
 
 }).$start();
